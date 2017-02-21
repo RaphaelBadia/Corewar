534d037224c7b3e819b92e8a47df7cc1e43d7219
Jacques YE <jye@e3r7p6.42.fr>
2017-02-21 22:35:18 +0100

    asd

--- src/main.c
+++ src/main.c
@@ -6,16 +6,23 @@
 /*   By: jye <jye@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/21 18:08:24 by jye               #+#    #+#             */
-/*   Updated: 2017/02/21 18:43:43 by jye              ###   ########.fr       */
+/*   Updated: 2017/02/21 22:35:03 by jye              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
+#include <string.h>
 #include <errno.h>
 #include <stdio.h>
 #include "vm.h"
 
 int		main(int ac, char **av)
 {
-	return (0);
+	/* TODO
+	** 		Magic header check
+	**		write binary shit into g_map
+	**		create all that function business
+	**		easy peazy
+	*/
+	return 0;
 }
--- src/vm.h
+++ src/vm.h
@@ -6,7 +6,7 @@
 /*   By: jye <jye@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/21 18:09:05 by jye               #+#    #+#             */
-/*   Updated: 2017/02/21 18:49:12 by jye              ###   ########.fr       */
+/*   Updated: 2017/02/21 22:35:11 by jye              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,65 @@
 # define VM_H
 # include "op.h"
 # define ERROR "Error"
+# define USAGE "%s [-dump cycle -n player_id] <champion.cor...>\n"
+	
 /*
+** The following flag should be added, -n -dump
+** -n [number] player
+** -dump dump vm at cycle
+**
 ** r1 == given out as player_id at launch
+** *******
 ** MEM_SIZE / player == offset between each player
+** *******
 ** if process has not live in CYCLE_TO_DIE, process will be killed
-** if process has live == NBR_LIVE -> CYCLE_TO_DIE will decrease by CYCLE_DELTA
-** game over when all process are dead
-** Player X (champion_name) won where X is the player’s number and champion_name is its name.
+** *******
+** 
+** *******
+** game is over when all process are dead
+** *******
+** Player X (champion_name) won where X is the player’s number and 
+** champion_name is its name.
+** *******
 ** A process shows that player X (champion_name) is alive
+** *******
+** live T_DIR-> T_DIR is the player number required for live
+** -------
+** ld T_DIR | T_IND , T_REG, for T_DIR as argument, directly stack as int, 
+**					  for T_IND as argument -> *(PC + (IND % IDX_MOD)) taking directly 
+**												4 bytes as its T_DIR
+** 
+** ldi T_REG | T_IND | T_DIR, T_DIR | T_REG, T_REG, 
+** *((1st param + 2nd param) % IDX_MOD) -> REG
+** or 
+** *((*(PC + T_IND % IDX_MOD) + 2nd param) % IDX_MOD) -> REG
+** 
 */
+
+typedef struct	s_champ
+{
+	char			*name;
+	char			*comment;
+	unsigned int	size;
+	unsigned int	last_live;
+	unsigned int	live;
+}				t_champ;
+
 typedef struct	s_statu
 {
-	int		nb_player;
-	int
+	unsigned int	nb_player;
+	unsigned long	nb_process;
+	unsigned long	cycle;
 }				t_statu
 
 typedef struct	s_process
 {
 	int		r[16];
+	int		idp_live;
+	int		ex_cycle;
 	int		pc;
 	int		carry;
 }				t_process;
 
+static char		g_map[MEM_SIZE] = {0};
 #endif
