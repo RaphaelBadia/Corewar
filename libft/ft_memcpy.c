a3e74393ea2e7b8bf4a357092749cb3bbf5117cf
Jacques YE <jye@e2r10p16.42.fr>
2017-03-20 21:13:02 +0100

    ft_memcpy chunk chunk as fuck

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -6,36 +6,45 @@
 /*   By: jye <marvin@42.fr>                         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/04 14:26:05 by jye               #+#    #+#             */
-/*   Updated: 2016/12/10 22:11:09 by jye              ###   ########.fr       */
+/*   Updated: 2017/03/20 20:55:16 by jye              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+static void	go_fast(unsigned char **cdst, unsigned char **csrc, size_t *n)
+{
+	unsigned long	*ldst;
+	unsigned long	*lsrc;
+	size_t			a;
+
+	ldst = (unsigned long *)*cdst;
+	lsrc = (unsigned long *)*csrc;
+	a = *n;
+	while (a >= 8)
+	{
+		*ldst++ = *lsrc++;
+		a -= 8;
+	}
+	*n = a;
+	*csrc = (unsigned char *)lsrc;
+	*cdst = (unsigned char *)ldst;
+}
+
 void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
-	unsigned long long	*ldst;
-	unsigned long long	*lsrc;
-	unsigned char		*cdst;
-	unsigned char		*csrc;
+	unsigned char	*cdst;
+	unsigned char	*csrc;
 
 	cdst = (unsigned char *)dst;
 	csrc = (unsigned char *)src;
-	ldst = NULL;
-	while (((unsigned long)cdst & (sizeof(unsigned long long) - 1)) && n)
+	while (((unsigned long)cdst & (sizeof(unsigned long) - 1)) && n)
 	{
 		*cdst++ = *csrc++;
 		--n;
 	}
 	if (n >= 8)
-	{
-		ldst = (unsigned long long *)cdst;
-		lsrc = (unsigned long long *)csrc;
-		while (n >= 8 && (*ldst++ = *lsrc++))
-			n -= 8;
-	}
-	csrc = ldst != NULL ? (unsigned char *)lsrc : csrc;
-	cdst = ldst != NULL ? (unsigned char *)ldst : cdst;
+		go_fast(&cdst, &csrc, &n);
 	while (n-- > 0)
 		*cdst++ = *csrc++;
 	return (dst);
